732f0d0d8a34be066bfdedcb7ddf393b36798d47
rpinto <rpinto@gmail.com>
2015-07-30 18:37:12 +0100

    HDP: added node 4

--- HDP_Conf.ps1
+++ HDP_Conf.ps1
@@ -24,7 +24,8 @@ $nodes = @(
     "\\vide-hadoopm03",
     "\\vide-hadoops01",
     "\\vide-hadoops02",
-    "\\vide-hadoops03"
+    "\\vide-hadoops03",
+    "\\vide-hadoops04"
 )
 if ($Commit -eq $True)
 {
@@ -35,7 +36,7 @@ if ($Commit -eq $True)
 }
 if ($Restart -eq $True) {
     Write-Host "$(Get-Date -Format HH:mm:ss) HDP_Conf: Stopping Hortonworks cluster..."
-    Invoke-Command -computername vide-hadoopm01,vide-hadoopm02,vide-hadoopm03,vide-hadoops01,vide-hadoops02,vide-hadoops03 {c:\hdp\stop_local_hdp_services.cmd}
+    Invoke-Command -computername vide-hadoopm01,vide-hadoopm02,vide-hadoopm03,vide-hadoops01,vide-hadoops02,vide-hadoops03,vide-hadoops04 {c:\hdp\stop_local_hdp_services.cmd}
     Write-Host "$(Get-Date -Format HH:mm:ss) HDP_Conf: Copying files to all cluster nodes"
     foreach ($file in $files) {
         foreach ($node in $nodes) {
@@ -48,6 +49,6 @@ if ($Restart -eq $True) {
     Start-Sleep -s 60
     Invoke-Command -computername vide-hadoopm02 {c:\hdp\start_local_hdp_services.cmd}
     Start-Sleep -s 30
-    Invoke-Command -computername vide-hadoops01,vide-hadoops02,vide-hadoops03 {c:\hdp\start_local_hdp_services.cmd}
+    Invoke-Command -computername vide-hadoops01,vide-hadoops02,vide-hadoops03,vide-hadoops04 {c:\hdp\start_local_hdp_services.cmd}
 }
 Write-Host "$(Get-Date -Format HH:mm:ss) HDP_Conf: All Done."
